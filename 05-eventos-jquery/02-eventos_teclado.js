ac0b3dc84ba4f7bc0e7990c9d20e2bf16b9ddeb8
DevJoaoSales <pmagal539@gmail.com>
2023-05-15 20:03:00 -0300

    update 5

---
+++ 05-eventos-jquery/02-eventos_teclado.js
@@ -0,0 +1,19 @@
+
+$(document).ready(()=>{
+
+    // Evento para quando pressionar uma tecla.
+    $("#form_login [name=username]").keydown(function(event){
+        console.log(`KEYDOWN - Você apertou  a tecla com um código ${event.keyCode}`)
+    }); 
+
+    /*// Evento para quando soltarmos uma tecla.
+    $("#form_login [name=username]").keyup(function(event){
+        console.log(`Você apertou a tecla com o codigo ${event.keyCode}`);
+    }); */
+
+    // Evento para quando uma tecla estiver sendo pressionada.
+    $("#form_login [name=username]").keypress(function(event){
+        console.log(`KEYPRESS - Você apertou a tecla com o código ${event.keyCode}`);
+    });
+
+});
